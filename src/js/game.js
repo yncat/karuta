1fa79276f7fd6968fc7513a3696e29e45ab46a85
nyanchan <personal@nyanchangames.com>
2020-03-08 20:49:51 +0900

    Frontend done

--- src/js/game.js
+++ src/js/game.js
@@ -1,9 +1,23 @@
 import {Howl, Howler} from 'howler';
 import {sounds} from './soundList'
+
 window.onload=function(){
-//var sound = new Howl({src: ['soundscat.ogg']});
-//sound.play();
-//sounds = fs.readdirSync('./sounds');
-//console.log(sounds);
-console.log("sound length:"+sounds.length)
+const lst=document.getElementById("soundlist_select");
+sounds.forEach((elem)=>{
+var e=document.createElement("option");
+e.setAttribute("value",elem);
+e.innerHTML=elem;
+lst.appendChild(e);
+});
+}
+
+var navigation_opened="false";
+
+window.onNavigationButtonClick=function(){
+navigation_opened = navigation_opened=="true" ? "false" : "true";
+document.getElementById("navigation_btn").setAttribute("aria-expanded",navigation_opened);
+document.getElementById("soundlist").className=navigation_opened=="true" ? "soundlist" : "soundlist closed";
+}
+
+window.onSoundListDecide=function(){
 }
